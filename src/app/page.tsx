6d8aeee5244583f1eae3f6b47edd49ca8d6592d9
biswajitmee <biswajit.mee@gmail.com>
2025-03-06 13:32:21 +0530

    Save local changes before merging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       {/* <ScrollTwoSection />
       <ScrollThreeSection /> */}
       <ScrollFourSection />
-      {/* <ScrollFiveSection /> */}
+      <ScrollFiveSection />
     </>
   )
 }
